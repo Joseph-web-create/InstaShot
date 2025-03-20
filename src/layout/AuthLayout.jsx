77a0b784e1e75ef6ba85857b762ffb2f3137cb64
Aace <aacejovi@gmail.com>
2025-03-20 14:30:17 +0100

    login and register page done

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,8 +1,17 @@
+import { Outlet } from "react-router";
+import home from "../assets/home.png";
 
 function AuthLayout() {
   return (
-    <div>AuthLayout</div>
-  )
+    <section className="container mx-auto grid grid-cols-12 items-center justify-center min-h-screen">
+      <div className="hidden lg:block col-span-6 mx-auto">
+        <img src={home} alt="AuthImage" />
+      </div>
+      <div className="col-span-12 lg:col-span-6 mx-auto">
+        <Outlet />
+      </div>
+    </section>
+  );
 }
 
-export default AuthLayout
+export default AuthLayout;
