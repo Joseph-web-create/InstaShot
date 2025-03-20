b18be2855a7a09caa4760589b96030afa1fb9ad2
Aace <aacejovi@gmail.com>
2025-03-20 15:53:10 +0100

    registration page started

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,19 @@
+import Logo from "../../assets/Logo.svg";
+import { Link } from "react-router";
+
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="w-[90vw] md:w-[500px] border rounded-md border-[#A1A1A1] py-[40px] px-[28px]">
+      <div className="flex justify-center">
+        <Link to="/">
+          <img src={Logo} alt="InstaShot logo" />
+        </Link>
+      </div>
+      <form>
+        
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
