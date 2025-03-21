59adfa3b6afc7df8b0c2ddc6d41417d508ab43e4
Aace <aacejovi@gmail.com>
2025-03-21 12:29:16 +0100

    form validation frontend

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,30 @@
 import Logo from "../../assets/Logo.svg";
 import { Link } from "react-router";
+import { useForm } from "react-hook-form";
+import {
+  validateEmail,
+  validatePassword,
+  validateUsername,
+  validatefullname,
+} from "../../utils/formvalidation";
+import { useState } from "react";
 
 function Register() {
+  const [revealPassword, setRevealPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const togglePassword = () => {
+    setRevealPassword((prev) => !prev);
+  };
+
+  const formSubmit = (e) => {
+    console.log(e);
+  };
+
   return (
     <div className="w-[90vw] md:w-[500px] border rounded-md border-[#A1A1A1] py-[40px] px-[28px]">
       <div className="flex justify-center">
@@ -9,8 +32,95 @@ function Register() {
           <img src={Logo} alt="InstaShot logo" />
         </Link>
       </div>
-      <form>
-        
+      <form
+        className="md:max-w-[400px] mx-auto mt-10"
+        onSubmit={handleSubmit(formSubmit)}
+      >
+        <div className="mb-4">
+          <label className="floating-label">
+            <span>Email</span>
+            <input
+              type="email"
+              placeholder="Email"
+              className="input input-lg w-full"
+              id="email"
+              {...register("email", {
+                validate: (value) => validateEmail(value),
+              })}
+            />
+          </label>
+          {errors.email && (
+            <span className="text-sm text-red-600">{errors.email.message}</span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label className="floating-label">
+            <span>FullName</span>
+            <input
+              type="text"
+              placeholder="Fullname"
+              className="input input-lg w-full"
+              id="fullname"
+              {...register("fullname", {
+                validate: (value) => validatefullname(value),
+              })}
+            />
+          </label>
+          {errors.fullname && (
+            <span className="text-sm text-red-600">
+              {errors.fullname.message}
+            </span>
+          )}
+        </div>
+        <div className="mb-4">
+          <label className="floating-label">
+            <span>UserName</span>
+            <input
+              type="text"
+              placeholder="Username"
+              className="input input-lg w-full"
+              id="username"
+              {...register("username", {
+                validate: (value) => validateUsername(value),
+              })}
+            />
+          </label>
+          {errors.username && (
+            <span className="text-sm text-red-600">
+              {errors.username.message}
+            </span>
+          )}
+        </div>
+        <div className="mb-4 relative">
+          <label className="floating-label">
+            <span>Password</span>
+            <input
+              type={revealPassword ? "password" : "text"}
+              placeholder="Username"
+              className="input input-lg w-full"
+              id="password"
+              {...register("password", {
+                validate: (value) => validatePassword(value),
+              })}
+            />
+          </label>
+          <button
+            className="absolute inset-y-0 right-2"
+            onClick={togglePassword}
+            type="button"
+          >
+            {revealPassword ? <button>Show</button> : <button>Hide</button>}
+          </button>
+        </div>
+        {errors.password && (
+          <span className="text-sm text-red-600">
+            {errors.password.message}
+          </span>
+        )}
+
+        <button className="mt-4 btn btn-secondary w-full" type="submit">
+          Continue
+        </button>
       </form>
     </div>
   );
