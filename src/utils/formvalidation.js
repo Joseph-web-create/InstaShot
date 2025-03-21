70f169715ea6726e0090156a245c9dcdce3eda30
Aace <aacejovi@gmail.com>
2025-03-21 12:33:48 +0100

    !

--- src/utils/formvalidation.js
+++ src/utils/formvalidation.js
@@ -11,7 +11,7 @@ export const validatePassword = (password) => {
   const validRegex = /^(?=.[a-z])(?=.[A-Z]).{5,}$/;
   if (!password) {
     return "Password is required";
-  } else if (!validRegex.test(password)) {
+  } else if (validRegex.test(password)) {
     return "Password must contain at least one lowercase, uppercase, and at least 5 characters long";
   }
 };
